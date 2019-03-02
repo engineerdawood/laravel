e313d2b995f176ea14e2ff39d48300d2ba910263
Muhammad Dawood <mdawoodch16@gmail.com>
2019-03-02 11:07:42 +0500

    this is index.php branch 1 change

--- index.php
+++ index.php
@@ -6,11 +6,13 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
 </head>
+<head>this is another head</head>
 <body>
     <h1>This is github Testing Phase</h1>
     <h1>This is aa change</h1>
     <h1>this is another change</h1>
     <h1>This is my read me file repository</h1>
     <h1>This is another change after first commit</h1>
+    <h1>This is a change from branch1</h1>
 </body>
 </html>
